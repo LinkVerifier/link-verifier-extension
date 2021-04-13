f4902d160e96a7e13beaea72b9bea60fdab1303b
Mateusz Niewiadomski <matt_nie@wp.pl>
2021-04-13 16:21:23 +0200

    add css injection

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ const sendURL = () => {
             type: 'content',
             url: currentURL,
         }, response => {
-            console.log(response.message);
+            alert(response.message);
         });
     }
 }
@@ -28,27 +28,30 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 });
 
+const alert = message => {
+    var URL = window.location.href;
+    var divId = 'link-verifier-c31d80e9-8bce-4d2a-b4ca-5d82c6536f1d';
+    var body = document.body;
+    var div = document.createElement('div');
+    div.id = divId;
+    div.textContent = URL
+    /*
+    //div styles
+    console.log(div);
+    document.body.appendChild(div);
+    div.style.all = 'unset';
+    div.style.position = 'absolute';
+    div.style.top = '0';
+    div.style.bottom = '0';
+    div.style.width = 'calc(100% - 40px)';
+    div.style.height = '20px';
+    div.style.zIndex = '10000';
+    div.style.margin = '10px';
+    div.style.padding = '10px';
+    div.style.borderRadius = '30px 30px 30px 30px';
+    div.style.backgroundColor = 'rgb(161, 0, 0)'
+    div.style.color = 'white';
+    */
+}
 
 
-/*var URL = window.location.href;
-var divId = 'link-verifier-c31d80e9-8bce-4d2a-b4ca-5d82c6536f1d';
-var body = document.body;
-var div = document.createElement('div');
-div.id = divId;
-div.textContent = URL
-//div styles
-
-console.log(div);
-document.body.appendChild(div);
-div.style.all = 'unset';
-div.style.position = 'absolute';
-div.style.top = '0';
-div.style.bottom = '0';
-div.style.width = 'calc(100% - 40px)';
-div.style.height = '20px';
-div.style.zIndex = '10000';
-div.style.margin = '10px';
-div.style.padding = '10px';
-div.style.borderRadius = '30px 30px 30px 30px';
-div.style.backgroundColor = 'rgb(161, 0, 0)'
-div.style.color = 'white';*/
